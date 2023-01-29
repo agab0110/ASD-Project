7b22a4d9c3fb98ba71d13f1d027c0421c0d0f7ac
agab0110 <agabrielex01@gmail.com>
2023-01-29 18:06:35 +0100

    added ui interface

--- src/main/java/com/monopoli/Main.java
+++ src/main/java/com/monopoli/Main.java
@@ -1,10 +1,14 @@
 package com.monopoli;
 
-import com.monopoli.gui.MainFrame;
+//import com.monopoli.gui.MainFrame;
+import com.monopoli.ui.MainTerminal;
 
 public class Main {
     public static void main(String[] args) {
-        MainFrame mainFrame = new MainFrame();
-        mainFrame.setVisible(true);
+        /*MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);*/
+
+        MainTerminal mainTerminal = new MainTerminal();
+        mainTerminal.showMenu();
     } 
 }
