fa966f2c223c3184217a99439cd3199e47caf373
agab0110 <agabrielex01@gmail.com>
2023-02-17 13:28:32 +0100

    added card class

---
+++ src/main/java/com/monopoli/app/Card.java
@@ -0,0 +1,14 @@
+package com.monopoli.app;
+
+public class Card {
+    private String desc;
+    
+    public Card(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+}
