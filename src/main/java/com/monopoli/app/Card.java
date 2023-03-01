6d86d04f65804d8847f4a606038c6fbbb88b86e5
agab0110 <agabrielex01@gmail.com>
2023-03-01 10:05:26 +0100

    created card specifices

--- src/main/java/com/monopoli/app/Card.java
+++ src/main/java/com/monopoli/app/Card.java
@@ -1,5 +1,18 @@
 package com.monopoli.app;
 
 public class Card {
+    String description;
+    int id;
+    public Card(String description, int id) {
+        this.description = description;
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
     
 }
