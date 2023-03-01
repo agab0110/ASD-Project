c6709e3a98a84260d029b50fcda73237b0e8b53f
agab0110 <agabrielex01@gmail.com>
2023-03-01 10:04:06 +0100

    changed class name

--- src/main/java/com/monopoli/app/Card.java
+++ src/main/java/com/monopoli/app/Card.java
@@ -1,14 +1,5 @@
 package com.monopoli.app;
 
 public class Card {
-    private String desc;
     
-    public Card(String desc) {
-        this.desc = desc;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
 }
