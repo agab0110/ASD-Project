8aaf350acc84944ff7febff81fdb4217edb80d52
agab0110 <agabrielex01@gmail.com>
2023-03-02 12:02:42 +0100

    added javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.monopoli.app","l":"Card"},{"p":"com.monopoli.app","l":"Contract"},{"p":"com.monopoli.app","l":"Menager"},{"p":"com.monopoli.app","l":"MoneyException"},{"p":"com.monopoli.app","l":"Player"},{"p":"com.monopoli.app","l":"PlayerException"}];updateSearchResults();
